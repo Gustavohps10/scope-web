8ddb165389dea45386c9820035d8676e3e0e5bc0
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-04-24 14:31:57 -0300

    feat: create user in database

--- api/src/main/factories/makeCreateUserController.ts
+++ api/src/main/factories/makeCreateUserController.ts
@@ -1,10 +1,10 @@
 import { Controller } from "../../presentation/contracts/controller"
 import { CreateUserService } from "../../data/services/createUser.service";
-import { PrismaRepository } from "../../infra/repositories/Prisma.repository";
+import { UserPrismaRepo } from "../../infra/repositories/PrismaRepo/User.prismarepo";
 import { CreateUserController } from "../../presentation/controllers/CreateUserController";
 
 export const makeCreateUserController = (): Controller =>{
-    const repo = new PrismaRepository()
+    const repo = new UserPrismaRepo()
     const service = new CreateUserService(repo)
     const controller = new CreateUserController(service);
     return controller;
