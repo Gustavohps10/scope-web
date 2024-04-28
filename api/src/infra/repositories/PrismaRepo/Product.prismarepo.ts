f67836dcfc651fff98471fb00d99a0181af93f29
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-04-28 13:29:29 -0300

    feat: add Product prisma repository

---
+++ api/src/infra/repositories/PrismaRepo/Product.prismarepo.ts
@@ -0,0 +1,77 @@
+import { ProductRepository } from "../../../data/contracts/productRepository";
+import { ProductDTO } from "../../../data/dto/ProductDTO";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export class ProductPrismaRepo implements ProductRepository {
+    async insert(productProps: ProductDTO): Promise<void> {
+        await prisma.produto.create({
+            data: {
+                DS_PRODUTO: productProps.description,
+                OBS_PRODUTO: productProps.observation,
+                VL_VENDA_PRODUTO: productProps.price,
+                DT_CADASTRO_PRODUTO: productProps.createAt,
+                STATUS_PRODUTO: productProps.status,
+                CATEGORIA: {
+                    connect:{
+                        CATEGORIAPRODUTOID: productProps.categoryId
+                    }
+                }
+            }
+        });
+    }
+
+    async findAll(): Promise<Required<ProductDTO[]>> {
+      const products = await prisma.produto.findMany()
+      return products.map(product => ({
+        id: product.PRODUTOID,
+        description: product.DS_PRODUTO,
+        observation: product.OBS_PRODUTO,
+        price: product.VL_VENDA_PRODUTO,
+        status: product.STATUS_PRODUTO as "ACTIVE" | "INACTIVE",
+        createAt: product.DT_CADASTRO_PRODUTO,
+        categoryId: product.CATEGORIAPRODUTOID
+      }));  
+    }
+
+    async findById(id: number): Promise<Required<ProductDTO>> {
+        const product = await prisma.produto.findFirstOrThrow();
+        return {
+            id: product.PRODUTOID,
+            description: product.DS_PRODUTO,
+            observation: product.OBS_PRODUTO,
+            price: product.VL_VENDA_PRODUTO,
+            status: product.STATUS_PRODUTO as "ACTIVE" | "INACTIVE",
+            createAt: product.DT_CADASTRO_PRODUTO,
+            categoryId: product.CATEGORIAPRODUTOID
+        }
+    }
+
+    async edit(productProps: ProductDTO): Promise<void> {
+        await prisma.produto.update({
+            where:{
+                PRODUTOID: productProps.id
+            },
+            data: {
+                DS_PRODUTO: productProps.description,
+                OBS_PRODUTO: productProps.observation,
+                VL_VENDA_PRODUTO: productProps.price,
+                DT_CADASTRO_PRODUTO: productProps.createAt,
+                STATUS_PRODUTO: productProps.status,
+                CATEGORIA: {
+                    connect:{
+                        CATEGORIAPRODUTOID: productProps.categoryId
+                    }
+                }
+            }  
+        })
+    }
+
+    async delete(id: number): Promise<void> {
+        await prisma.produto.delete({
+            where:{
+                PRODUTOID: id
+            }
+        })
+    }
+}
