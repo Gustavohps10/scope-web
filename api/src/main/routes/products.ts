618fc73923f913dabe2b622dcbe20948a8250a56
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-04-28 16:25:19 -0300

    feat: add find all products endpoint

--- api/src/main/routes/products.ts
+++ api/src/main/routes/products.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { adaptRoute } from "../adapters/expressRouterAdapter";
 import { makeCreateProductController } from "../factories/makeCreateProductController";
+import { makeFindAllProductsController } from "../factories/makeFindAllProductsController";
 
 export default (router: Router) =>{
+    router.get('/products', adaptRoute(makeFindAllProductsController()))
     router.post('/products', adaptRoute(makeCreateProductController()))
 }
