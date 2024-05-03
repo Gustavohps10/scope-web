1a4d081416db3d92c248106d85b6216b9aa02385
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-05-02 21:08:19 -0300

    feat: add custom errors in product category repository

--- api/src/infra/repositories/PrismaRepo/ProductCategory.prismarepo.ts
+++ api/src/infra/repositories/PrismaRepo/ProductCategory.prismarepo.ts
@@ -1,54 +1,79 @@
 import { ProductCategoryRepository } from "../../../data/contracts/productCategoryRepository";
 import { ProductCategoryDTO } from "../../../data/dto/ProductCategoryDTO";
 import { PrismaClient } from "@prisma/client";
+import { PrismaError } from "../../../main/helpers/PrismaError";
 const prisma = new PrismaClient()
 
 export class ProductCategoryPrismaRepo implements ProductCategoryRepository{
+    private readonly entityName = "Product Category";
+
     async insert(productCategoryProps: ProductCategoryDTO): Promise<void> {
-        await prisma.categoria_produto.create({
-            data: {
-                DS_CATEGORIA_PRODUTO: productCategoryProps.description
-            }
-        });
+        try {
+            await prisma.categoria_produto.create({
+                data: {
+                    DS_CATEGORIA_PRODUTO: productCategoryProps.description
+                }
+            });
+        } catch (error) {
+            throw new PrismaError(error, "Failed to create product category", this.entityName)
+        }
     }
 
     async findById(id: number): Promise<Required<ProductCategoryDTO>> {
-        const category = await prisma.categoria_produto.findUniqueOrThrow({
-            where: {
-                CATEGORIAPRODUTOID: id
+        try {
+            const category = await prisma.categoria_produto.findUniqueOrThrow({
+                where: {
+                    CATEGORIAPRODUTOID: id
+                }
+            });
+    
+            return {
+                id: category.CATEGORIAPRODUTOID,
+                description: category.DS_CATEGORIA_PRODUTO
             }
-        });
-
-        return {
-            id: category.CATEGORIAPRODUTOID,
-            description: category.DS_CATEGORIA_PRODUTO
+        } catch (error) {
+            throw new PrismaError(error, "Failed to find product category by Id", this.entityName)
         }
     }
 
     async findAll(): Promise<Required<ProductCategoryDTO[]>> {
-        const categories = await prisma.categoria_produto.findMany();
-        return categories.map(category => ({
-            id: category.CATEGORIAPRODUTOID,
-            description: category.DS_CATEGORIA_PRODUTO
-        }))
+        try {
+            const categories = await prisma.categoria_produto.findMany();
+            return categories.map(category => ({
+                id: category.CATEGORIAPRODUTOID,
+                description: category.DS_CATEGORIA_PRODUTO
+            }))
+        } catch (error) {
+            throw new PrismaError(error, "Failed to find all product categories", this.entityName)    
+        }
+        
     }
 
     async edit(productCategoryProps: ProductCategoryDTO): Promise<void> {
-        await prisma.categoria_produto.update({
-            data: {
-                DS_CATEGORIA_PRODUTO: productCategoryProps.description
-            },
-            where: {
-                CATEGORIAPRODUTOID: productCategoryProps.id
-            }
-        })
+        try {
+            await prisma.categoria_produto.update({
+                data: {
+                    DS_CATEGORIA_PRODUTO: productCategoryProps.description
+                },
+                where: {
+                    CATEGORIAPRODUTOID: productCategoryProps.id
+                }
+            })
+        } catch (error) {
+            throw new PrismaError(error, "Failed to update product category", this.entityName)    
+        }
     }
 
     async delete(id: number): Promise<void> {
-        await prisma.categoria_produto.delete({
-            where: {
-                CATEGORIAPRODUTOID: id
-            }
-        })
+        try {
+            await prisma.categoria_produto.delete({
+                where: {
+                    CATEGORIAPRODUTOID: id
+                }
+            })
+        } catch (error) {
+            throw new PrismaError(error, "Failed to delete product category", this.entityName)    
+        }
+        
     }
 }
