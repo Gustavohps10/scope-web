c4a4c142d99b2f11a2b2ada097cdd96090dac69f
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-05-06 23:49:57 -0300

    chore: downgrade eslint version to ^8.41.0

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import eslintnextConfig from "eslint-config-next"
 
 
 export default [
@@ -9,17 +10,5 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
-  {
-    extends: [
-    "next/core-web-vitals",
-    "plugin:react-hooks/recommended"
-    ],
-    plugins: [
-      "react-hooks"
-    ],
-    rules: {
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn"
-    }
-  }
+  eslintnextConfig
 ];
